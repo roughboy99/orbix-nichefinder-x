14636ebb9e4c6413481d9b255c85c922fd9dfed9
Hector Diaz <66765645+roughboy99@users.noreply.github.com>
2026-03-25 19:06:55 -0400

    v1.07: fix parseUser fields, remove Content-Type header, fix false update notification

--- installer/INSTALL.bat
+++ installer/INSTALL.bat
@@ -1,10 +1,10 @@
 @echo off
-title Orbix NicheFinder X - Installer v1.06
+title Orbix NicheFinder X - Installer v1.07
 color 0B
 
 echo.
 echo  ============================================================
-echo   ORBIX NicheFinder X  v1.06  ^|  Windows Installer
+echo   ORBIX NicheFinder X  v1.07  ^|  Windows Installer
 echo   Orbix Automation Solutions  ^|  getorbix.com
 echo  ============================================================
 echo.
