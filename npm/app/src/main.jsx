992eeeecbd148d79ba95a116d506f306ecc84997
Hector Diaz <66765645+roughboy99@users.noreply.github.com>
2026-03-25 20:14:22 -0400

    feat: add npm package structure (bin/nichefinder.js CLI, app template)

---
+++ npm/app/src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NicheFinderX from './orbix-nichefinder-x.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><NicheFinderX /></React.StrictMode>
+)
